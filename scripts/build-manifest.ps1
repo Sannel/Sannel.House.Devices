1a60f6276591b01a3326115f65094aed9f7eae34
Adam Holt <holtsoftware@outlook.com>
2019-01-21 22:04:20 -0700

    fixed build-manifest issue.

--- scripts/build-manifest.ps1
+++ scripts/build-manifest.ps1
@@ -7,21 +7,21 @@ $version = GetVersion
 $buildType = SetBuildType
 $imageName = GetImageName
 
-$combinedTag="${env:DOCKER_REGISTRY}$imageName:$buildType-$version"
-$arm="${env:DOCKER_REGISTRY}$imageName:$buildType-$version-linux-arm"
-$x64="${env:DOCKER_REGISTRY}$imageName:$buildType-$version-linux-x64"
-$win="${env:DOCKER_REGISTRY}$imageName:$buildType-$version-win"
+$combinedTag="${env:DOCKER_REGISTRY}${imageName}:$buildType-$version"
+$arm="${env:DOCKER_REGISTRY}${imageName}:$buildType-$version-linux-arm"
+$x64="${env:DOCKER_REGISTRY}${imageName}:$buildType-$version-linux-x64"
+$win="${env:DOCKER_REGISTRY}${imageName}:$buildType-$version-win"
 
 docker manifest create $combinedTag $arm $x64 $win
 docker manifest push $combinedTag
 
-$combinedTag="${env:DOCKER_REGISTRY}$imageName:$buildType"
+$combinedTag="${env:DOCKER_REGISTRY}${imageName}:$buildType"
 docker manifest create $combinedTag $arm $x64 $win
 docker manifest push $combinedTag
 
 if($buildType -eq "release")
 {
-	$combinedTag="${env:DOCKER_REGISTRY}$imageName:latest"
+	$combinedTag="${env:DOCKER_REGISTRY}${imageName}:latest"
 	docker manifest create $combinedTag $arm $x64 $win
 	docker manifest push $combinedTag
 }
