525f1860c90050a0edb5f5107053557b11222a5f
Adam Holt <holtsoftware@outlook.com>
2019-01-23 19:13:43 -0700

    Updated build-manifest.

--- scripts/build-manifest.ps1
+++ scripts/build-manifest.ps1
@@ -7,22 +7,22 @@ $version = GetVersion
 $buildType = SetBuildType
 $imageName = GetImageName
 
-$combinedTag="${env:DOCKER_REGISTRY}${imageName}:$buildType-$version"
-$arm="${env:DOCKER_REGISTRY}${imageName}:$buildType-$version-linux-arm"
-$x64="${env:DOCKER_REGISTRY}${imageName}:$buildType-$version-linux-x64"
-$win="${env:DOCKER_REGISTRY}${imageName}:$buildType-$version-win"
-
-TryLogin
-docker manifest create $combinedTag $arm $x64 $win
-docker manifest push $combinedTag
-
-$combinedTag="${env:DOCKER_REGISTRY}${imageName}:$buildType"
-docker manifest create $combinedTag $arm $x64 $win
-docker manifest push $combinedTag
+$data = ". `"`$PSScriptRoot/_common.ps1`"`n";
+$data = $data + "`$combinedTag=`"${env:DOCKER_REGISTRY}${imageName}:$buildType-$version`"`n";
+$data = $data + "`$arm=`"${env:DOCKER_REGISTRY}${imageName}:$buildType-$version-linux-arm`"`n";
+$data = $data + "`$x64=`"${env:DOCKER_REGISTRY}${imageName}:$buildType-$version-linux-x64`"`n";
+$data = $data + "`$win=`"${env:DOCKER_REGISTRY}${imageName}:$buildType-$version-win`"`n";
+$data = $data + "TryLogin`n";
+$data = $data + "docker manifest create `$combinedTag `$arm `$x64 `$win`n";
+$data = $data + "docker manifest push `$combinedTag`n";
+$data = $data + "`$combinedTag=`"${env:DOCKER_REGISTRY}${imageName}:$buildType`"`n";
+$data = $data + "docker manifest create `$combinedTag `$arm `$x64 `$win`n";
+$data = $data + "docker manifest push `$combinedTag`n";
 
 if($buildType -eq "release")
 {
-	$combinedTag="${env:DOCKER_REGISTRY}${imageName}:latest"
-	docker manifest create $combinedTag $arm $x64 $win
-	docker manifest push $combinedTag
+	$data = $data + "`$combinedTag=`"${env:DOCKER_REGISTRY}${imageName}:latest`"`n";
+	$data = $data + "docker manifest create `$combinedTag `$arm `$x64 `$win`n";
+	$data = $data + "docker manifest push `$combinedTag`n";
 }
+$data | Out-File "$PSScriptRoot/push-manifest.ps1" -Encoding utf8 -Force
