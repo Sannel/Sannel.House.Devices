43264b07d7204056a6e68a2f6669678be0662135
Adam Holt <holtsoftware@outlook.com>
2019-01-23 19:26:21 -0700

    Fix syntax error in script generation.

--- scripts/build-push.ps1
+++ scripts/build-push.ps1
@@ -10,7 +10,7 @@ $imageName = GetImageName
 $data = ". `"`$PSScriptRoot/_common.ps1`"`n"
 $data = $data + "`$image=`"${env:DOCKER_REGISTRY}${imageName}:$buildType-$version-`"`n";
 $data = $data + "TryLogin`n";
-$data = $data + "`$image= `$image + GetArchString`n";
+$data = $data + "`$image= `$image + (GetArchString)`n";
 $data = $data + "return docker push `$image";
 
 $data | Out-File "$PSScriptRoot/push-image.ps1" -Encoding utf8 -Force
